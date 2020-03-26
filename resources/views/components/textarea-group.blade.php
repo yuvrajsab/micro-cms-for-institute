0afa7e0938e3ccdad55e4392032c440aae17b029
Yuvraj Sablania <sablaniayuvraj@gmail.com>
2020-03-26 18:47:52 +0530

    fix textarea blade component

--- resources/views/components/textarea-group.blade.php
+++ resources/views/components/textarea-group.blade.php
@@ -2,7 +2,10 @@
     <label for="{{ $id ?? '' }}">{{ $title }}</label>
 
     <textarea class="form-control {{ $classes ?? '' }} @error($name) is-invalid @enderror" id="{{ $id ?? '' }}"
-        name="{{ $name }}" {{ $attributes ?? '' }}>{{ old($name, $slot ?? '') }}</textarea>
+        name="{{ $name }}" @php foreach ($attributes ?? [] as $key=> $value) {
+                echo "$key=\"{$value}\"";
+            }
+        @endphp>{{ old($name, $slot ?? '') }}</textarea>
 
     @error($name)
     <div class="invalid-feedback">{{ $message }}</div>
