77bd5e6b811549962d569b7bb68f3614ded04c6c
Yuvraj Sablania <sablaniayuvraj@gmail.com>
2020-04-01 15:52:29 +0530

    add posts relation in category

--- app/Category.php
+++ app/Category.php
@@ -3,13 +3,21 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Category extends Model
 {
+    use SoftDeletes;
+
     protected $fillable = ['name', 'slug', 'description', 'created_by'];
 
     public function creator()
     {
         return $this->belongsTo(User::class, 'created_by');
+    }
+
+    public function posts()
+    {
+        return $this->hasMany(Post::class);
     }
 }
