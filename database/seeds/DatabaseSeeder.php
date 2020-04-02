82b16f2ca5542b2f4bb8d74164e407265fbdbea5
Yuvraj Sablania <sablaniayuvraj@gmail.com>
2020-04-02 20:05:12 +0530

    fix seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +13,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@admin.com',
+            'password' => Hash::make('password'),
+        ]);
+
+        $this->call(PostsTableSeeder::class);
     }
 }
--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -14,7 +14,7 @@ class PostsTableSeeder extends Seeder
     public function run()
     {
         factory(Category::class, 10)->create()->each(function ($category) {
-            factory(Post::class, 2)->create(['category_id', $category->id]);
+            $category->posts()->save(factory(Post::class)->make(['category_id' => $category->id]));
         });
     }
 }
